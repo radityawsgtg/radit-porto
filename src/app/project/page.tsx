bffd066962bf963778fa7d5e5924218d8dfb938a
Radityawsgtg <radityaws79@gmail.com>
2026-01-16 23:45:02 +0700

    feat: adding title to the page

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -11,11 +11,20 @@ import Navbar from "@/components/Navbar";
 
 
 
+const handron = Handron({ 
+  src: '../../../public/fonts/Handron-Solid.otf',
+  variable: '--font-handron'
+});
 
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-roboto',
+});
+
+const eureka = Eureka({ 
+  src: '../../../public/fonts/Euskadi-Regular.otf',
+  variable: '--font-eureka'
 });
 
 
@@ -26,6 +35,8 @@ export default function ProjectPage() {
     return (
         <main className="relative min-h-screen w-full pt-20">
             <Navbar />
+            <h2 className={`text-[48px] font-bold text-center mb-10 z-10 relative text-[#FFD88C] [text-shadow:0_3px_19px_#FFD88C50] ${handron.className}`}>PROJECTS</h2>
+
             {/* 1. LAYER BACKGROUND: Gambar memenuhi seluruh area main */}
             <div className="absolute inset-0 z-0 bg-[#17052A]">
                 <Image
@@ -40,8 +51,8 @@ export default function ProjectPage() {
                 <h1 className={`text-4xl font-bold mb-6 ${roboto.className}`}>My Projects</h1>
                 {/* Tambahkan daftar proyek atau konten lainnya di sini */}
             </div>
-            <div className="rounded-l-2xl bg-yellow-300 w-1 h-1.5"> // buat comit aja lolll
-
+            <div className="rounded-l-2xl bg-yellow-300 w-1 h-1.5">     
+                <h1 className="text-black">.</h1>
             </div>
 
         </main>
