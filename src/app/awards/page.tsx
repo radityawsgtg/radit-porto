90fb8105084e10a5b20babf39255f4718d51d931
Radityawsgtg <radityaws79@gmail.com>
2026-01-20 16:41:43 +0700

    feat: make awards

---
+++ src/app/awards/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+import Handron from "next/font/local";
+import Eureka from "next/font/local";
+import { Roboto } from "next/font/google";
+import { use } from "react";
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
+import { GitHubCalendar } from "react-github-calendar";
+import ProjectCard from "@/components/project";
+import CardPhoto from "@/components/cardphoto";
+
+
