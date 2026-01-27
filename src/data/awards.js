eaff6b1432319b082f1303045ce0bad8af830b60
Radityawsgtg <radityaws79@gmail.com>
2026-01-27 22:22:51 +0700

    feat: adding data

--- src/data/awards.js
+++ src/data/awards.js
@@ -2,23 +2,44 @@
 export const AwardsData = [
     {
         id: 1,
-        title: "JUARA 1 WEB DESIGN",
-        description: "Berhasil memenangkan kompetisi tingkat nasional dengan proyek inovatif berbasis Next.js dan Tailwind CSS.",
-        narasumber: "Nasional IT Fest 2024",
+        title: "Awardee of Beasiswa Unggulan 2025-2029 fully funded scholarship.",
+        description: "I was awarded the Beasiswa Unggulan Kemendikbudristek 2025-2029 fully funded scholarship for undergraduate students. Received a fully funded scholarship for undergraduate studies. Recognized for academic excellence and potential",
+        narasumber: "Kemendikbudristek",
         image: "/Rectangle 5873.png"
     },
     {
         id: 2,
-        title: "BEST FRONTEND DEV",
-        description: "Penghargaan atas implementasi UI/UX terbaik dalam ajang hackathon internasional di Singapura.",
-        narasumber: "Global Tech Award",
+        title: "Winner of National Ocean Engineering Competition ARUNGI 2024",
+        description: "In 2024, I led a team of students in the Ocean Engineering Competition ARUNGI, where we developed innovative solutions to address various offshore challenges. Our project focused on sustainable practices and advanced technologies to improve efficiency and stability in marine operations. Through extensive research, collaboration, and prototyping, we successfully presented our ideas to a panel of experts and emerged as the competition winners.",
+        narasumber: "Keluarga Mahasiswa Teknik Kelautan ITB",
         image: "/Rectangle 5873.png"
     },
     {
         id: 3,
-        title: "CERTIFIED DEVELOPER",
-        description: "Sertifikasi profesional dari Google untuk keahlian dalam pengembangan aplikasi web skala besar.",
-        narasumber: "Google Developer Group",
+        title: "Member of Parlemen Remaja DIY 2023",
+        description: "I was selected as a member of Parlemen Remaja DIY 2023, where I represented youth voices in discussions on education and culture. Through collaboration with students from various schools, I actively contributed ideas, particularly on bullying and women’s rights and provided input that supported the government’s policy formulation process. ",
+        narasumber: "Parlemen Remaja DIY 2023",
         image: "/Rectangle 5873.png"
-    }
+    },
+    {
+        id: 4,
+        title: "Chairman of Forum Orchestra Pelajar Edukatif Kota Yogyakarta (Forte)",
+        description: "In 2025, the Collaboration Concert of Yogyakarta Student Orchestra became a landmark event showcasing the solidarity and creativity of students across the city. Under the leadership of the General Chair, the concert successfully established an effective workflow that brought together six schools: SMAN 1 Teladan Yogyakarta, SMAN 3 Padmanaba Yogyakarta, SMAN 2 Yogyakarta, SMAN 8 Yogyakarta, SMK 2 Kasihan, and SMA Stella Duce I. The event also expanded its reach by collaborating with DR. Nathania Karina (Thrust Orchestra from Jakarta. ",
+        narasumber: "Dinas Kebudayaan Provinsi DIY",
+        image: "/Rectangle 5873.png"
+    },
+    {
+        id: 5,
+        title: "Staff of Mandiri Jogja Marathon 2024",
+        description: " The Mandiri Jogja Marathon is an annual international running event held in Yogyakarta, Indonesia. It combines sports, culture, and tourism, featuring scenic routes through villages, temples, and rice fields. Runners from around the world join to experience both athletic challenge and the unique cultural charm of Jogja.",
+        narasumber: "Bank Mandiri",
+        image: "/Rectangle 5873.png"
+    },
+    {
+        id: 6,
+        title: "Chairman HUT Teladan 66",
+        description: "In 2024–2025, as the General Chair of the 66th Teladan Anniversary, I led a large-scale celebration that successfully united students, stakeholders, and the wider community. The event featured a sold-out theater performance involving seven extracurricular clubs, a talk show with the Rector of Universitas Gadjah Mada, and innovative programs like Teladan Action Day, widely covered by five newspapers. By coordinating 234 committee members, restructuring the organization for greater efficiency, and managing event finances effectively, the team achieved significant impact while generating sustainable profits for future programs.",
+        narasumber: "SMAN 1 Teladan Yogyakarta",
+        image: "/Rectangle 5873.png"
+    },
     ];
